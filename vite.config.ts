bd8904f23b35ed594d40a1d42f6ddf2b4cbef4f1
Maninder Singh <birdimaninder4@gmail.com>
2024-06-22 19:14:47 +0530

    initial code commit.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'http://gogreen.com/',
+  use: {
+    ctViteConfig: {
+      plugins: [react()],
+    },
+  },
 })
