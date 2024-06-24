74f850bb5a7c7f5be6062088cc887c8e753e30a9
Maninder Singh <birdimaninder4@gmail.com>
2024-06-24 10:02:58 +0530

    site URL removed.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  use: {
-    ctViteConfig: {
-      plugins: [react()],
-    },
-  },
+  plugins: [react()],
 })
